a2aa224268796eab6d1d91e2ac4bb683355a19fa
JensenKim <87356942+Jensenkim@users.noreply.github.com>
2021-12-13 17:58:33 +0900

    최종작업본

--- shop/src/main/java/com/shop/dto/CartOrderDto.java
+++ shop/src/main/java/com/shop/dto/CartOrderDto.java
@@ -1,2 +1,16 @@
-package com.shop.dto;public class CartOrderDto {
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CartOrderDto {
+
+    private Long cartItemId;
+
+    private List<CartOrderDto> cartOrderDtoList;
+
 }
